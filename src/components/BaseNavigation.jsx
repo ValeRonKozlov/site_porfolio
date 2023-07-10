93cd8e6e6ed0017425e0a7944b1c866ab4c1f938
Valeron <valer4ik007@rambler.ru>
2023-07-10 19:37:56 +0300

    add new files and do some style changes

--- src/components/BaseNavigation.jsx
+++ src/components/BaseNavigation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-import "../styles/navigation.css"
+import "../styles/Navigation.css"
 
 
 function BaseNavigation() {
@@ -12,19 +12,19 @@ function BaseNavigation() {
   return (
     <nav className="header__nav">
       <p className="nav-tag">
-        <Link to="/">
+        <Link to="/" className="item-link tag-link">
         &lt; / ValeRon Kozlov &gt;
         </Link>
       </p>
       <ul className="nav-list">
         <li className="list-item">
-          <Link to="/about">{t("main.nav.about me")}</Link>
+          <Link className="item-link" to="/about">{t("hero.nav.about me")}</Link>
         </li>
         <li className="list-item">
-          <Link to="/works">{t("main.nav.work")}</Link>
+          <Link className="item-link" to="/works">{t("hero.nav.work")}</Link>
         </li>
         <li className="list-item">
-          <Link to="/kontakt">{t("main.nav.kontakt")}</Link>
+          <Link className="item-link" to="/kontakt">{t("hero.nav.kontakt")}</Link>
         </li>
       </ul>
     </nav>
